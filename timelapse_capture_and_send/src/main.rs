e9a82be0b6dd696a26b2393c87715ebb9221e3d5
joe tracey <joetracey1995@gmail.com>
2026-01-25 00:08:21 -0500

    added send single image to client

--- timelapse_capture_and_send/src/main.rs
+++ timelapse_capture_and_send/src/main.rs
@@ -1,11 +1,24 @@
 use nexigo_lib;
+use image_handling;
 
+const CURRENT_FORMAT: image_handling::ImageFormat = image_handling::ImageFormat::YUYV;
+
+// This edition just captures a single image,sends it, then exits.
 fn main() {
     let camera = nexigo_lib::Camera::new(
         1280 as u32,
         720 as u32,
     );
     let dev_path = "/dev/video0";
-    let yuvu_shot = camera.take_picture(dev_path.to_string());
-    println!("Hello, world!");
+    let yuyv_result = camera.take_picture(dev_path.to_string());
+    let yuyv_shot = yuyv_result.expect("Failed to take picture");
+    println!("Hello, world! We have a picture");
+    image_handling::send_image(
+       1280 as u32,
+       720 as u32,
+       CURRENT_FORMAT,
+       yuyv_shot,
+       String::from("test_shot"),
+       String::from("<address>"),
+    );
 }
