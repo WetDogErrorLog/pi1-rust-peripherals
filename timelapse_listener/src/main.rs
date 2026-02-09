b1be93d56dd956f94ca7920810ebe7b13b8ab7ae
joe tracey <joetracey1995@gmail.com>
2026-02-09 00:19:06 -0500

    changed listener to use config

--- timelapse_listener/src/main.rs
+++ timelapse_listener/src/main.rs
@@ -5,21 +5,28 @@ use axum::{
     response::IntoResponse,
     Router,
 };
+use serde::{Serialize, Deserialize};
 use std::net::SocketAddr;
 use std::sync::Arc;
 use image_handling::{CameraPacket, handle_image_post};
+use serde_json;
 
-
-// TODO: Replace with reading from local config.
+#[derive(Serialize, Deserialize, Debug)]
 struct AppState{
-    storage_dir: String,
+    storage_dir_path: String,
+    service_port: u16,
 }
 
 #[tokio::main]
 async fn main() {
-    let shared_state = Arc::new(AppState {
-        storage_dir: "/home/shared_space/timelapse_data".to_string(),
-    });
+    let listener_str = std::fs::read_to_string("./listener_config.json")
+        .expect("failed to read config to string");
+    let listener_config: AppState = serde_json::from_str(&listener_str)
+        .expect("failed to unpack the json");
+    let storage_dir = listener_config.storage_dir_path.clone();     
+    let service_port = listener_config.service_port.clone();  
+
+    let shared_state = Arc::new(listener_config);
     
     // TODO: When we swap away from YUYV we can reduce body limit to default.
     let app = Router::new()
@@ -27,7 +34,7 @@ async fn main() {
         .layer(DefaultBodyLimit::max(15 * 1024 * 1024))
         .with_state(shared_state);
 
-    let addr = SocketAddr::from(([0,0,0,0], 3000));
+    let addr = SocketAddr::from(([0,0,0,0], service_port));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     println!("Listening..."); 
     axum::serve(listener, app).await.unwrap();
@@ -39,7 +46,7 @@ async fn upload_handler(
     Json(packet): Json<CameraPacket>,
 ) -> impl IntoResponse {
     println!("Recieved image {}, {}", packet.project_folder, packet.file_name_root);
-    match handle_image_post(packet, &state.storage_dir){
+    match handle_image_post(packet, &state.storage_dir_path){
         Ok(_) => {
             println!("saved");
             (StatusCode::OK, "Save success")
